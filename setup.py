372fa1f3e9fee37f28aa5ef83e4197a6939a8838
Jeremie Bigras Dunberry <bigjerbd@gmail.com>
2018-07-22 00:34:13 +0900

    fix replace readme and setup with proper one

--- setup.py
+++ setup.py
@@ -6,18 +6,18 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name='pydarg',
+    name='csv2neo4jcsv',
     version='0.1',
-    description='Pure Python decorator for more complex dictionary handling',
+    description='Util to convert a csv to a neo4j csv',
     author='Jeremie Bigras-Dunberry',
     author_email='Bigjerbd@gmail.com',
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
-    url='https://github.com/BigJerBD/pydarg',
+    url='https://github.com/BigJerBD/csv2neo4jcsv',
     license='MIT License',
     platforms=['POSIX', 'Windows', 'Unix', 'MacOS'],
-    keywords=['python', 'dictionary', 'decorator', 'configuration', 'utils'],
+    keywords=['python', 'csv','Neo4j'],
     classifiers=(
         "Programming Language :: Python :: 3.6",
         "Operating System :: OS Independent",
